637f729b5cf0b41598f26104bfb3ed11b620b8a8
Goran Grbic <tpojka@gmail.com>
2020-02-10 16:34:31 +0100

    Removed unused method.

--- app/Http/Controllers/Api/Profile/IndexController.php
+++ app/Http/Controllers/Api/Profile/IndexController.php
@@ -9,14 +9,14 @@ use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Log;
+use Log;
 
 class IndexController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return void
      */
     public function index()
     {
@@ -26,7 +26,7 @@ class IndexController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return Response
+     * @return void
      */
     public function create()
     {
@@ -37,7 +37,7 @@ class IndexController extends Controller
      * Store a newly created resource in storage.
      *
      * @param Request $request
-     * @return Response
+     * @return void
      */
     public function store(Request $request)
     {
@@ -48,18 +48,18 @@ class IndexController extends Controller
      * Display the specified resource.
      *
      * @param User $user
-     * @return Response
+     * @return JsonResponse
      */
     public function show(User $user)
     {
-        //
+        return response()->json(request()->user(), 200);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param User $user
-     * @return Response
+     * @return void
      */
     public function edit(User $user)
     {
@@ -92,15 +92,10 @@ class IndexController extends Controller
      * Remove the specified resource from storage.
      *
      * @param User $user
-     * @return Response
+     * @return void
      */
     public function destroy(User $user)
     {
         //
-    }
-
-    public function updateProfile(Request $request)
-    {
-        return response()->json(['existing_user' => $request->user(), 'requested_changes' => $request->all()], 200);
     }
 }
